a367cb7a0cc00effd95d2d640aa72b50c4df744f
AbraxasInnovations <derekmogi@gmail.com>
2025-09-26 21:26:50 -0400

    mobile layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -41,7 +41,7 @@ export default function RootLayout({
         {/* Google AdSense */}
         <script
           async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-XXXXXXXXXXXXXXXX"
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7684892898477318"
           crossOrigin="anonymous"
         ></script>
       </head>
